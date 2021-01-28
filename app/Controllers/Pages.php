2c0c0ab8a98fb06cb481cdd991139da8b13de4f4
amandazk <zacharukaz@gmail.com>
2021-01-28 10:44:15 -0300

    Adding logic to the controller

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Controllers;
+
+use CodeIgniter\CodeIgniter;
 use CodeIgniter\Controller;
 
 class Pages extends Controller
@@ -12,5 +14,17 @@ class Pages extends Controller
 
     public function view($page = 'home') // accepts one argument named $page
     {
+        if( ! is_file(APPPATH.'/Views/pages/'.$page.'.php'))
+        {
+            // we don't have a page for that
+            throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
+        }
+
+        $data['title'] = ucfirst($page); // capitalize the first letter
+        
+        // loading the views in the order they should be displayed
+        echo view('templates/header', $data);
+        echo view('pages/'.$page, $data);
+        echo view('templates/footer', $data);
     }
 }
---
+++ app/Views/pages/home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Home</title>
+</head>
+
+<body>
+
+    <h1>Hello. This is the HomePage</h1>
+
+</body>
+
+</html>
